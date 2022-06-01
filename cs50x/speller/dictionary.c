eaa171c2a52310a4c701d2948a4e9a56db4456d0
Ravi Dayabhai <ravi.dayabhai@chime.com>
2022-06-01 03:04:26 -0700

    Cook up better hash function for `speller`

--- cs50x/speller/dictionary.c
+++ cs50x/speller/dictionary.c
@@ -13,19 +13,19 @@
 // Represents a node in a hash table
 typedef struct node
 {
-   char        word[LENGTH + 1];
-   struct node *next;
+	char word[LENGTH + 1];
+	struct node *next;
 }
 node;
 
-// Prototype for helper functions
+// Prototypes for helper functions
 bool unload_linked_list(node *head);
 
 // Global to track number of words loaded into dictionary
 unsigned int loaded_words = 0;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+// Choose number of buckets in hash table
+const unsigned int N = 1000000;
 
 // Hash table
 node *table[N] = { NULL };
@@ -36,102 +36,101 @@ node *table[N] = { NULL };
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-   node *head = table[hash(word)];
+	node *head = table[hash(word)];
 
-   while (head)
-   {
-      if (strcasecmp(head->word, word) == 0)
-      {
-         return true;
-      }
-      head = head->next;
-   }
+	while (head)
+	{
+		if (strcasecmp(head->word, word) == 0)
+		{
+			return true;
+		}
+		head = head->next;
+	}
 
-   return false;
+	return false;
 }
 
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-   // TODO: Improve this hash function
-   return toupper(word[0]) - 'A';
+	return (((strlen(word) * toupper(word[1])) + toupper(word[0])) << 5) % N;
 }
 
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-   /* Open file */
-   char word[LENGTH + 1];
-   FILE *file = fopen(dictionary, "r");
+	/* Open file */
+	char word[LENGTH + 1];
+	FILE *file = fopen(dictionary, "r");
 
-   if (!file)
-   {
-      perror("Error in opening dictionary file!");
-      return false;
-   }
+	if (!file)
+	{
+		perror("Error in opening dictionary file!");
+		return false;
+	}
 
-   /* Read dictionary into hash table line-wise */
-   while (fscanf(file, "%s", word) != EOF)
-   {
-      unsigned int h  = hash(word);
-      node         *w = (node *)malloc(sizeof(node));
+	/* Read dictionary into hash table line-wise */
+	while (fscanf(file, "%s", word) != EOF)
+	{
+		unsigned int h  = hash(word);
+		node         *w = (node *)malloc(sizeof(node));
 
-      if (!w)
-      {
-         perror("Unable to allocate memory from heap to 'word' node!");
-         return false;
-      }
+		if (!w)
+		{
+			perror("Unable to allocate memory from heap to 'word' node!");
+			return false;
+		}
 
-      strcpy(w->word, word);
-      w->next  = table[h];
-      table[h] = w;
+		strcpy(w->word, word);
+		w->next  = table[h];
+		table[h] = w;
 
-      ++loaded_words;
-   }
+		++loaded_words;
+	}
 
-   /* Close file */
-   if (fclose(file))
-   {
-      perror("Error in closing dictionary file!");
-      return false;
-   }
+	/* Close file */
+	if (fclose(file))
+	{
+		perror("Error in closing dictionary file!");
+		return false;
+	}
 
-   return true;
+	return true;
 }
 
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-   return loaded_words;
+	return loaded_words;
 }
 
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-   for (int h = 0, n = sizeof(table) / sizeof(node *); h < n; ++h)
-   {
-      if ((table[h]) && (unload_linked_list(table[h]) == false))
-      {
-         return false;
-      }
-   }
+	for (int h = 0; h < (int)N; ++h)
+	{
+		if ((table[h]) && (unload_linked_list(table[h]) == false))
+		{
+			return false;
+		}
+	}
 
-   return true;
+	return true;
 }
 
 
 // Recursive helper function to unload linked list from memory
 bool unload_linked_list(node *head)
 {
-   if (head->next)
-   {
-      unload_linked_list(head->next);
-   }
+	if (head->next)
+	{
+		unload_linked_list(head->next);
+	}
 
-   free(head);
-   return true;
+	free(head);
+	return true;
 }
